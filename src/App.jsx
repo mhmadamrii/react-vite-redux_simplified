a5724867ad5bfaee006e7bf60eaf99cafdc98245
mhmadamrii <interceptorghost4@gmail.com>
2023-10-18 19:48:34 +0700

    add some config

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,8 @@
-import React from "react";
-import "./index.css";
-import { useSelector } from "react-redux";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { routes } from "./routes";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-/* screens */
-import Playground from "./screens/vim-playground";
-import Auth from "./screens/authentication";
-import NotFoundPage from "./screens/not-found-page";
-import Navbar from "./components/navbar";
-
-// const myURL = "https://express-creation.vercel.app/api/v1";
-
-export default function App() {
-  const user = useSelector((state) => state.user);
-  const client = new QueryClient();
-
+export default function App(props) {
+  console.log('props', props)
   return (
-    <QueryClientProvider client={client}>
-      <Router>
-        {user.isLoggedIn && <Navbar />}
-        <Routes>
-          {user.isLoggedIn ? (
-            <>
-              {routes.map((route, index) => (
-                <Route key={index} path={route.path} element={<route.component />} />
-              ))}
-            </>
-          ) : (
-            <>
-              <Route path="/login" element={<Auth />} />
-              <Route path="/" element={<Playground />} />
-              <Route path="/*" element={<NotFoundPage />} />
-            </>
-          )}
-        </Routes>
-      </Router>
-    </QueryClientProvider>
-  );
+    <>
+      <h4>Checking rendering</h4>
+    </>
+  )
 }
-
-/* url tutorial */
-/* 
-  https://programmingfields.com/how-to-create-react-todo-app-using-redux-for-beginners/
-*/
--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import "./index.scss";
-import { Provider } from "react-redux";
+import {Provider} from "react-redux";
 import store from "./redux/store/index.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
