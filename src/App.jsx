f207a9b4d7795cb78709262b45b17c4e3df311b1
mhmadamrii <interceptorghost4@gmail.com>
2023-10-18 20:44:22 +0700

    react query installation

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,39 @@
 import React from "react";
-import "./index.css";
-import { useSelector } from "react-redux";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { routes } from "./routes";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {QueryClientProvider, QueryClient, useQuery} from "@tanstack/react-query";
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
 
-/* screens */
-import Playground from "./screens/vim-playground";
-import Auth from "./screens/authentication";
-import NotFoundPage from "./screens/not-found-page";
-import Navbar from "./components/navbar";
+const Something = () => {
+  const {isPending, error, data, isLoading} = useQuery({
+    queryKey: ["repoData"],
+    queryFn: () => fetch("https://jsonplaceholder.typicode.com/posts?_limit=20").then((res) => res.json()),
+  });
 
-// const myURL = "https://express-creation.vercel.app/api/v1";
+  console.log("data", data);
+  console.log("error", error);
+  if (isLoading) {
+    return <h1>Loading...................</h1>;
+  }
+  return (
+    <div>
+      <h1>Hello world!</h1>
+    </div>
+  );
+};
 
 export default function App() {
-  const user = useSelector((state) => state.user);
-  const client = new QueryClient();
-
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+        refetchOnMount: false,
+        retry: 1,
+      },
+    },
+  });
   return (
-    <QueryClientProvider client={client}>
-      <Router>
-        {user.isLoggedIn && <Navbar />}
-        <Routes>
-          {user.isLoggedIn ? (
-            <>
-              {routes.map((route, index) => (
-                <Route key={index} path={route.path} element={<route.component />} />
-              ))}
-            </>
-          ) : (
-            <>
-              <Route path="/login" element={<Auth />} />
-              <Route path="/" element={<Playground />} />
-              <Route path="/*" element={<NotFoundPage />} />
-            </>
-          )}
-        </Routes>
-      </Router>
+    <QueryClientProvider client={queryClient}>
+      <Something />
+      <ReactQueryDevtools />
     </QueryClientProvider>
   );
 }
-
-/* url tutorial */
-/* 
-  https://programmingfields.com/how-to-create-react-todo-app-using-redux-for-beginners/
-*/
